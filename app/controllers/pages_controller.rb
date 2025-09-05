45eb3c8885dcd32b448e154fdd42f4758a791145
Akira TSURUDA <jas39.gripen@gmail.com>
2025-09-05 18:17:49 +0900

    Add sample ERB pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# PagesController: static pages with simple ERB examples
+class PagesController < ApplicationController
+  def about
+    @frameworks = %w[Rails Hanami Sinatra]
+  end
+
+  def contact
+    @contact_methods = %w[Email Phone Chat]
+  end
+
+  def create_contact
+    redirect_to contact_path, notice: 'Message sent'
+  end
+end
+
